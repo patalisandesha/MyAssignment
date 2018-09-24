150481852e22923384bdd31955a7959773db62a1
patalisandesha <42207372+patalisandesha@users.noreply.github.com>
2018-09-24 16:31:36 +0530

    Add files via upload

---
+++ RestaurantApp/RestaurantApp/Desserts.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantte
+{
+    class Desserts
+    {
+        private Dictionary<string, int> dess_dict = new Dictionary<string, int>()
+        {
+            {"Smoothie",100 },
+            {"Strawberry Smoothie",50},
+            {"Chocolate cake",250 }
+
+        };
+
+        public int items()
+        {
+          
+                Menu ob = new Menu();
+                return ob.itemselector(dess_dict);
+            
+        }
+    }
+}
---
+++ RestaurantApp/RestaurantApp/MainDish.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurantte
+{
+    public class MainDish : Products
+    {
+        private Dictionary<string, int> main_dict = new Dictionary<string, int>()
+        {
+            {" Veg Pallav ",100},
+            {"Rice Bath",50 },
+            {" Dosa ",75 }
+
+        };
+
+        public int items()
+        {
+            Menu obj = new Menu();
+            return obj.itemselector(main_dict);
+        }        
+    }
+}
